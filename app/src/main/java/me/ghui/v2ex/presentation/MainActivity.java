5cefbf0e73e3a797d25e11df81a81226dd43c9fb
ghui <ghuiii.zhang@gmail.com>
2016-12-14 00:19:01 +0800

    add dependencies

--- app/src/main/java/me/ghui/v2ex/presentation/MainActivity.java
+++ app/src/main/java/me/ghui/v2ex/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package me.ghui.v2ex;
+package me.ghui.v2ex.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.ghui.v2ex.R;
 
 public class MainActivity extends AppCompatActivity {
 
