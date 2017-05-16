d2612a0402cb68bbb150db584c194c20924b6351
ghui <ghuiii.zhang@gmail.com>
2017-05-16 23:51:26 +0800

    add careinfo

--- app/src/main/java/me/ghui/v2ex/network/bean/CareInfo.java
+++ app/src/main/java/me/ghui/v2ex/network/bean/CareInfo.java
@@ -2,7 +2,9 @@ package me.ghui.v2ex.network.bean;
 
 import java.util.List;
 
+import me.ghui.fruit.Attrs;
 import me.ghui.fruit.annotations.Pick;
+import me.ghui.v2ex.util.Utils;
 
 /**
  * Created by ghui on 12/05/2017.
@@ -19,11 +21,69 @@ public class CareInfo {
     public static class Item {
         @Pick("img.avatar")
         private String avatar;
+        @Pick("strong a[href^=/member/]")
+        private String userName;
+        @Pick(value = "span.small.fade", attr = Attrs.OWN_TEXT)
+        private String time;
         @Pick("span.item_title a[href^=/t/]")
         private String title;
-        @Pick(value = "span.item_title a[href^=/t/]", attr = "href")
+        @Pick(value = "span.item_title a[href^=/t/]", attr = Attrs.HREF)
         private String link;
         @Pick("a.count_livid")
         private String comentNum;
+        @Pick("a.node")
+        private String tagName;
+        @Pick(value = "a.node", attr = Attrs.HREF)
+        private String tagLink;
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "avatar='" + avatar + '\'' +
+                    ", userName='" + userName + '\'' +
+                    ", time='" + getTime() + '\'' +
+                    ", title='" + title + '\'' +
+                    ", link='" + link + '\'' +
+                    ", comentNum='" + comentNum + '\'' +
+                    ", tagName='" + tagName + '\'' +
+                    ", tagLink='" + tagLink + '\'' +
+                    '}';
+        }
+
+        public String getTime() {
+            //  •  36 天前  •  最后回复来自
+            if (!Utils.isEmpty(time)) {
+                return time.trim().split("•")[1].trim();
+            }
+            return time;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public String getComentNum() {
+            return comentNum;
+        }
+
+        public String getTagName() {
+            return tagName;
+        }
+
+        public String getTagLink() {
+            return tagLink;
+        }
     }
 }
