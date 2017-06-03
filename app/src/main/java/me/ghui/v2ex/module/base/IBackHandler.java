32a96015a22e18e4ed4c7a3efe2f6459b6b0210b
ghui <ghuiii.zhang@gmail.com>
2017-06-03 16:26:15 +0800

    update

--- app/src/main/java/me/ghui/v2ex/module/base/IBackHandler.java
+++ app/src/main/java/me/ghui/v2ex/module/base/IBackHandler.java
@@ -4,6 +4,9 @@ package me.ghui.v2ex.module.base;
  * Created by ghui on 03/06/2017.
  */
 
+/**
+ * Something which can handle a backpress event
+ */
 public interface IBackHandler {
     void handleBackable(IBackable backable);
 }
--- app/src/main/java/me/ghui/v2ex/module/base/IBackable.java
+++ app/src/main/java/me/ghui/v2ex/module/base/IBackable.java
@@ -4,6 +4,9 @@ package me.ghui.v2ex.module.base;
  * Created by ghui on 03/06/2017.
  */
 
+/**
+ * Something which need to handle a backpress event
+ */
 public interface IBackable {
     void onBackPressed();
 }
