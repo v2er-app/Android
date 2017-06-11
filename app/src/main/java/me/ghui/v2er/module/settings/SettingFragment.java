3f571967d866553d83379f1a134533ad2e134502
ghui <ghuiii.zhang@gmail.com>
2017-06-11 22:31:10 +0800

    update

--- app/src/main/java/me/ghui/v2er/module/settings/SettingFragment.java
+++ app/src/main/java/me/ghui/v2er/module/settings/SettingFragment.java
@@ -37,8 +37,6 @@ public class SettingFragment extends PreferenceFragment implements Preference.On
         if (list != null) {
             list.setDivider(null);
         }
-
-
     }
 
     @Override
