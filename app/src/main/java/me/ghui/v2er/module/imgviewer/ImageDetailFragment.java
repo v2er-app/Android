673b42f4f91a87ce6a2fd00b9be3d86fc49d01ab
ghui <ghuiii.zhang@gmail.com>
2017-07-10 20:06:12 +0800

    update

--- app/src/main/java/me/ghui/v2er/module/imgviewer/ImageDetailFragment.java
+++ app/src/main/java/me/ghui/v2er/module/imgviewer/ImageDetailFragment.java
@@ -5,11 +5,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.graphics.Palette;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -19,8 +21,10 @@ import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.orhanobut.logger.Logger;
 
 import me.ghui.v2er.R;
+import me.ghui.v2er.module.home.MainActivity;
 import me.ghui.v2er.network.Constants;
 import me.ghui.v2er.util.Utils;
 
@@ -32,6 +36,7 @@ public class ImageDetailFragment extends Fragment {
     private String mImageUrl;
     private SubsamplingScaleImageView mImageView;
     private ProgressBar progressBar;
+    private View mImgRootView;
     private static String IMG_URL = Constants.PACKAGE_NAME + "_img_url";
 
     public static ImageDetailFragment newInstance(String url) {
@@ -57,6 +62,7 @@ public class ImageDetailFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mImgRootView = view.findViewById(R.id.img_viewer_root);
         progressBar = (ProgressBar) view.findViewById(R.id.loading);
         mImageView = (SubsamplingScaleImageView) view.findViewById(R.id.imageview);
         mImageView.setDoubleTapZoomStyle(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER);
@@ -79,6 +85,7 @@ public class ImageDetailFragment extends Fragment {
                     public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
                         mImageView.setImage(ImageSource.bitmap(resource));
                         progressBar.setVisibility(View.GONE);
+                        paletteBg(resource);
                     }
 
                     @Override
@@ -86,6 +93,19 @@ public class ImageDetailFragment extends Fragment {
                         progressBar.setVisibility(View.GONE);
                         Utils.toast("图片加载出错");
                     }
+                });
+    }
+
+    private void paletteBg(Bitmap bitmap) {
+        Palette.from(bitmap)
+                .generate(palette -> {
+                    Palette.Swatch textSwatch = palette.getVibrantSwatch();
+                    if (textSwatch == null) {
+                        Logger.e("textSwatch is null");
+                        return;
+                    }
+                    mImgRootView.setBackgroundColor(textSwatch.getRgb());
+//                    titleColorText.setTextColor(textSwatch.getTitleTextColor());
                 });
     }
 
