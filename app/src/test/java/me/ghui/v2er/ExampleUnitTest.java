cd814c46852d0eedf9f5b3ff9152a3477741da8c
ghui <ghuiii.zhang@gmail.com>
2017-08-15 19:31:43 +0800

    update pay logo to black

--- app/src/test/java/me/ghui/v2er/ExampleUnitTest.java
+++ app/src/test/java/me/ghui/v2er/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package me.ghui.v2er;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,5 +16,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testFindDigital() {
+        String text = "你有100元红包";
+        Pattern pattern = Pattern.compile("\\d[\\d.,]*(?=元)");
+        Matcher matcher = pattern.matcher(text);
+        String result = matcher.group();
+        assertEquals("100", result);
     }
 }
