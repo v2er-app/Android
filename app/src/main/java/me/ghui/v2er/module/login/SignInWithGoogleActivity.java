8cd632415a1f1fa9cfad89b0164dcc4596335f38
ghui <ghuiii.zhang@gmail.com>
2017-08-15 11:35:04 +0800

    update support lib to 26.0.1

---
+++ app/src/main/java/me/ghui/v2er/module/login/SignInWithGoogleActivity.java
@@ -0,0 +1,11 @@
+package me.ghui.v2er.module.login;
+
+import me.ghui.v2er.module.general.WapActivity;
+
+/**
+ * Created by ghui on 13/08/2017.
+ */
+
+public class SignInWithGoogleActivity extends WapActivity {
+    // TODO: 13/08/2017  
+}
