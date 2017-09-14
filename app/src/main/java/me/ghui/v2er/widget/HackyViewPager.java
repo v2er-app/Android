94a4f94f41963d1c685b0e729788d3dd25096e8e
ghui <ghuiii.zhang@gmail.com>
2017-09-14 14:43:40 +0800

    update

---
+++ app/src/main/java/me/ghui/v2er/widget/HackyViewPager.java
@@ -0,0 +1,33 @@
+package me.ghui.v2er.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by ghui on 14/09/2017.
+ */
+
+public class HackyViewPager extends ViewPager {
+    public HackyViewPager(Context context) {
+        super(context);
+    }
+
+    public HackyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            //uncomment if you really want to see these errors
+            //e.printStackTrace();
+            return false;
+        }
+    }
+
+}
