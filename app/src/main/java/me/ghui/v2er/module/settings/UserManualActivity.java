e3ad1207af4a3dee81058c50b540bda8f99e6f65
ghui <ghuiii.zhang@gmail.com>
2019-06-22 22:13:11 +0800

    UserManualActivity.java

--- app/src/main/java/me/ghui/v2er/module/settings/UserManualActivity.java
+++ app/src/main/java/me/ghui/v2er/module/settings/UserManualActivity.java
@@ -9,7 +9,8 @@ import me.ghui.v2er.module.topic.HtmlView;
 import me.ghui.v2er.util.Utils;
 import me.ghui.v2er.widget.BaseToolBar;
 
-public class UserManualActivity extends BaseActivity {
+public class UserManualActivity extends BaseActivity implements HtmlView.OnHtmlRenderListener {
+    private final String KEY_SCROLL_Y = KEY("key_scroll_y");
     @BindView(R.id.htmlview)
     HtmlView mHtmlView;
 
@@ -28,12 +29,24 @@ public class UserManualActivity extends BaseActivity {
     @Override
     protected void init() {
         super.init();
+        mHtmlView.setOnHtmlRenderListener(this);
         String localContent = Assets.getString("html/user_manual.html", this);
         mHtmlView.loadContentView(localContent);
     }
 
     @Override
     protected void reloadMode(int mode) {
-        ColorModeReloader.target(this).reload();
+        ColorModeReloader.target(this)
+                .putExtra(KEY_SCROLL_Y, mHtmlView.getScrollY())
+                .reload();
+    }
+
+    @Override
+    public void onRenderCompleted() {
+        int scrollY = getIntent().getIntExtra(KEY_SCROLL_Y, -1);
+        if (scrollY != -1) {
+            mHtmlView.scrollTo(0, scrollY);
+        }
+
     }
 }
