e271324a3a1b53355ef7a5457dd02113059c804d
zack <shiqizhenyes@gmail.com>
2021-12-24 17:26:30 +0800

    修复首页搜索按钮不显示
    修复首页搜索按钮不显示

--- app/src/main/java/me/ghui/v2er/module/home/MainActivity.java
+++ app/src/main/java/me/ghui/v2er/module/home/MainActivity.java
@@ -1,6 +1,7 @@
 package me.ghui.v2er.module.home;
 
 import android.annotation.SuppressLint;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -80,17 +81,24 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
         return null;
     }
 
-    @SuppressLint({"CheckResult", "WrongConstant"})
-    protected void configToolBar() {
-        mToolbar.setOnDoubleTapListener(this);
-        mToolbar.setTitle("");
-        mLogoView.setVisibility(View.VISIBLE);
-        mToolbar.setViewTileCenter(mLogoView);
+    @Override
+    protected void configToolBar(BaseToolBar toolBar) {
+        super.configToolBar(toolBar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
     public int attachOptionsMenuRes() {
         return R.menu.main_toolbar_menu;
+    }
+
+    @Override
+    public void configOptionsMenu(Menu menu) {
+        super.configOptionsMenu(menu);
+        mToolbar.setOnDoubleTapListener(this);
+        mToolbar.setTitle("");
+        mLogoView.setVisibility(View.VISIBLE);
+        mToolbar.setViewTileCenter(mLogoView);
     }
 
     @Override
@@ -133,7 +141,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     @Override
     protected void init() {
         isAlive = true;
-        configToolBar();
         BottomNavigationViewHelper.setImageSize(mBottomNavigationView,
                 getResources().getDimensionPixelSize(R.dimen.bottom_navigation_view_icon_small_size),
                 getResources().getDimensionPixelSize(R.dimen.bottom_navigation_view_icon_small_size));
