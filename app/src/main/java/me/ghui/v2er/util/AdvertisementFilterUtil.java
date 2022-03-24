d1b1b74a4c5431042c71a182b1884815e5cc2ba4
zack <shiqizhenyes@gmail.com>
2022-03-24 12:52:54 +0800

    移除页面中的谷歌广告
    移除页面中的谷歌广告

---
+++ app/src/main/java/me/ghui/v2er/util/AdvertisementFilterUtil.java
@@ -0,0 +1,22 @@
+package me.ghui.v2er.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import me.ghui.v2er.R;
+
+public class AdvertisementFilterUtil {
+
+    public static String clearAdvertisementDivJs(Context context){
+        StringBuilder js = new StringBuilder("javascript:");
+        Resources res = context.getResources();
+        String[] adDivs = res.getStringArray(R.array.advertisementBlockDiv);
+        for(int i=0;i<adDivs.length;i++){
+            js.append("var adDiv").append(i).append("= document.getElementById('").append(adDivs[i]).append("');if(adDiv").append(i).append(" != null)adDiv").append(i).append(".parentNode.removeChild(adDiv").append(i).append(");");
+        }
+        for(int i=0;i<adDivs.length;i++){
+            js.append("var adDiv").append(i).append("= document.getElementsByClassName('").append(adDivs[i]).append("');if(adDiv").append(i).append(" != null)adDiv").append(i).append(".parentNode.removeChild(adDiv").append(i).append(");");
+        }
+        return js.toString();
+    }
+}
