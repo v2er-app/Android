55629697c7f5bb4e8ac86cfa84ca70e10f9151c3
Gray Zhang <ghuiii.zhang@gmail.com>
2025-09-18 23:33:41 +0800

    chore: remove unused imports from MyAppGlideModule
    - Removed DecodeFormat import that is no longer used
    - Removed RequestOptions import that is no longer used
    - These imports were left over after fixing the Glide recursive initialization issue

--- app/src/main/java/me/ghui/v2er/general/MyAppGllideModule.java
+++ app/src/main/java/me/ghui/v2er/general/MyAppGllideModule.java
@@ -9,10 +9,8 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
-import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.io.InputStream;
 
